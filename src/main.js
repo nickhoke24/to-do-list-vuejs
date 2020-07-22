6a1a2daeb1013ac27f9b8b792a688afca5858aa3
nickhoke24 <51420208+nickhoke24@users.noreply.github.com>
2020-07-22 14:58:29 -0400

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {
+  MdField
+} from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/black-green-light.css'
+
+Vue.use(VueMaterial);
+Vue.use(MdField);
 
 Vue.config.productionTip = false
 
